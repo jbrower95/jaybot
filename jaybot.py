b1f37650c85dbe19e393f6b714d89e8c6a6d8f83
Justin Brower <jbrower95@gmail.com>
2020-03-17 17:51:37 -0400

    change jaybot

--- jaybot.py
+++ jaybot.py
@@ -11,10 +11,11 @@ import asyncio
 from tempfile import NamedTemporaryFile
 from gtts import gTTS
 import multiprocessing as mp
+import speech_recognition as sr
 
 # set this to the token for the bot.
 token = os.environ['JAY_BOT']
-
+r = sr.Recognizer()
 
 def loadFromFile(fp):
     with open(fp, "r") as f:
@@ -22,11 +23,25 @@ def loadFromFile(fp):
     return lines
 
 
-
 class JayClient(discord.Client):
 
     OWNER = 'your.guy.jay'
     CHANNEL = 'jaybot-commands'
+
+    HELP = '''
+    Welcome to JayBot! (tm)
+
+    JayBot introduces people when they join or leave a channel.
+
+    Available commands:
+
+    /silence - Shutup Jay. To unsilence, type it again.
+    /ranked - Enable ranked mode. It tells people to shut up while enabled.
+    /summon - If you're in a voice chat, brings Jay Bot to your chat.
+    /insult <person> - Tag someone to insult them!
+    /kill - Remove JayBot from the current voice chat.
+    /help - Print this menu.
+    '''
 
     def __init__(self):
         super().__init__()
@@ -74,25 +89,47 @@ class JayClient(discord.Client):
     async def on_message(self, message):
         print(message.channel.name)
         if message.channel.name == JayClient.CHANNEL:
-            if message.content is not None and "/roll" in message.content:
+            full_cmd = message.content.split()
+            if not full_cmd: return
+            cmd = full_cmd[0]
+            if cmd == "/roll":
                 parts = message.content.split(" ")
                 result = random.randint(1, 101)
                 await message.channel.send(f'Rolled {result}!')
-            if message.content == '/silence':
-                self.silenced = True
-                await message.channel.send(f'Silenced! To re-enable, type /enable')
-            if message.content == '/enable':
-                self.silenced = False
-                await message.channel.send(f'Enabled! To disable, type /silence')
-            if message.content == '/ranked':
+            if cmd == '/silence':
+                self.silenced = not self.silenced
+                await message.channel.send(f'{"Silenced!" if self.silenced else "Not silenced!"} To toggle, type /silence')
+            if cmd == '/ranked':
                 self.ranked = not self.ranked
-                await message.channel.send(f'Ranked mode enabled: {self.ranked}')
-            if message.content == '/summon':
+                await message.channel.send(f'Ranked mode: {"enabled" if self.ranked else "disabled"}')
+            if cmd == '/summon':
                 author = message.author.name
                 await self.attach_to_user(author)
-            if message.content == '/kill':
+            if cmd == '/kill':
                 await self.kill_all()
                 self.load_data()
+            if cmd == '/insult':
+                if not self.guild:
+                    return
+                args = full_cmd[1:]
+                if not args:
+                    await message.channel.send(f'Error: Tag someone to insult them!')
+                person = full_cmd[1]
+                person_id = int(person[3:-1])
+                target_member = None
+                # look up this person.
+                for member in self.guild.members:
+                    if member.id == person_id:
+                        # Found the member.
+                        target_member = member
+                        break
+                if not target_member:
+                    await message.channel.send(f'Error: Couldnt find that user!')
+                await self.speak_sync(random.choice(self.insults).format(member.name))
+
+
+            if message.content == '/help':
+                await message.channel.send(JayClient.HELP)
 
     async def kill_all(self):
         if self.connection:
@@ -107,6 +144,7 @@ class JayClient(discord.Client):
         self.departures = loadFromFile(path.join('data', 'departures.txt'))
         self.ranked_messages = loadFromFile(path.join('data', 'ranked.txt'))
         self.welcomes = loadFromFile(path.join('data', 'greetings.txt'))
+        self.insults = loadFromFile(path.join('data', 'insults.txt'))
 
     async def attach_to_user(self, name):
         if self.connection:
@@ -116,6 +154,7 @@ class JayClient(discord.Client):
         self.channel = None
         self.silenced = True
         for guild in self.guilds:
+            print(f"Found guild: {guild.name}")
             channels = guild.voice_channels
             for channel in channels:
                 members = channel.members
@@ -136,6 +175,7 @@ class JayClient(discord.Client):
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+
         await self.attach_to_user(JayClient.OWNER)
         if not self.connection:
             print("WARN: Tried to start JayBot without your.guy.jay in a voice chat.")
